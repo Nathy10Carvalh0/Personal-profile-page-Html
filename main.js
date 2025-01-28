5572f74889bd83d2fa100fd13ffd86f5512ff076
nathy10carvalh0 <nathy10carvalho@gmail.com>
2025-01-28 01:45:41 -0300

    update

--- main.js
+++ main.js
@@ -25,7 +25,27 @@ const translations = {
       sectionAboutH2: "ABOUT", // Section titles and descriptions
       sectionAboutH3: "Nathalia Carvalho, Frontend Developer & UX Designer",
       sectionAboutP: "Graduated in Systems Analysis since 2022, with postgraduate degrees in web applications and software engineering. Currently, I work as a Front-end developer, focusing on developing modern, high-quality interfaces. My expertise includes performance, user experience, and responsiveness.",
+      AboutStatsH3_1: "4 years",
+      AboutStatsP_1:"of experience in field",
+      AboutStatsH3_2: "21",
+      AboutStatsP_2:"repositories in Github",
+      AboutStatsH3_1: "4",
+      AboutStatsP_1:"companies worked",
+      ButtonDownloadResume:"Download resume",
   
+      SectionQualificationsH2:"Qualifications",
+      SectionQualificationsP:"My personal journey",
+      ButtonWork:"Work",
+      ButtonEducation:"Education",
+      WorkTabH3_1:"JR Systems Analyst",
+      WorkTabP_1:"Bradesco Bank",
+      WorkTabH3_2:"Intern",
+      WorkTabP_2:"Bradesco Bank",
+      WorkTabH3_3:"Data Processing Analyst",
+      WorkTabP_3:"Erodata",
+      WorkTabH3_4:"Trainee",
+      WorkTabP_4:"Itau bank",
+
       sectionTechnologyH2: "Technology", // Technologies section
       sectionTechnologyP: "Over the course of my academic life and career, I have used various tools to develop solutions, with the main ones being:",
       divSkillsColumnH3: {
@@ -35,6 +55,7 @@ const translations = {
       },
       sectionPortifolioH2: "My portifolio",
       contactH2: "CONTACT", // Contact section title
+      rights:"© all rights reserved.",
     },
     pt: {
       headerIntroText: "Início", // Translations for the same sections in Portuguese
@@ -49,6 +70,26 @@ const translations = {
       sectionAboutH2: "SOBRE", // Section titles and descriptions in Portuguese
       sectionAboutH3: "Nathalia Carvalho, Desenvolvedora Frontend & UX Designer",
       sectionAboutP: "Graduada em Análise de Sistemas desde 2022, com pós-graduação em aplicações web e engenharia de software. Atualmente, trabalho como desenvolvedora Front-end, focando em criar interfaces modernas e de alta qualidade. Minha experiência inclui desempenho, experiência do usuário e responsividade.",
+      AboutStatsH3_1: "4 anos",
+      AboutStatsP_1:"de experiência na Área",
+      AboutStatsH3_2: "21",
+      AboutStatsP_2:"repositorios no Github",
+      AboutStatsH3_3: "4",
+      AboutStatsP_3:"empresas trabalhadas",
+      ButtonDownloadResume:"Baixar Curriculo",
+
+      SectionQualificationsH2:"Qualificações",
+      SectionQualificationsP:"Minha Jornada Profissinal",
+      ButtonWork:"Trabalho",
+      ButtonEducation:"Formação",
+      WorkTabH3_1:"Analista de sistemas Junior",
+      WorkTabP_1:"Banco Bradesco",
+      WorkTabH3_2:"Intern",
+      WorkTabP_2:"Banco Bradesco",
+      WorkTabH3_3:"Analista de Processamento de Dados",
+      WorkTabP_3:"Erodata",
+      WorkTabH3_4:"Jovem Aprendiz",
+      WorkTabP_4:"Banco Itau",
   
       sectionTechnologyH2: "TECNOLOGIAS", // Technologies section in Portuguese
       sectionTechnologyP: "Ao longo da minha vida acadêmica e carreira, utilizei diversas ferramentas para desenvolver soluções, sendo as principais delas:",
@@ -59,6 +100,7 @@ const translations = {
       },
       sectionPortifolioH2: "Meu portifolio",
       contactH2: "CONTATO", // Contact section title in Portuguese
+      rights:"© Todos os direitos reservados.",
     },
   };
   
@@ -84,7 +126,26 @@ const translations = {
     document.getElementById("sectionAboutH2").innerText = t.sectionAboutH2;  // Update the "About" section title
     document.getElementById("sectionAboutH3").innerText = t.sectionAboutH3;  // Update the "About" section subtitle
     document.getElementById("sectionAboutP").innerText = t.sectionAboutP;  // Update the "About" section description
-  
+    document.getElementById("AboutStatsH3_1").innerText = t.AboutStatsH3_1;
+    document.getElementById("AboutStatsP_1").innerText = t.AboutStatsP_1;
+    document.getElementById("AboutStatsH3_2").innerText = t.AboutStatsH3_2;
+    document.getElementById("AboutStatsP_2").innerText = t.AboutStatsP_2;
+    document.getElementById("AboutStatsH3_3").innerText = t.AboutStatsH3_3;
+    document.getElementById("AboutStatsP_3").innerText = t.AboutStatsP_3;
+    document.getElementById("ButtonDownloadResume").innerText = t.ButtonDownloadResume;
+    document.getElementById("SectionQualificationsH2").innerText = t.SectionQualificationsH2;
+    document.getElementById("SectionQualificationsP").innerText = t.SectionQualificationsP;
+    document.getElementById("ButtonWork").innerText = t.ButtonWork;
+    document.getElementById("ButtonEducation").innerText = t.ButtonEducation;
+    document.getElementById("WorkTabH3_1").innerText = t.WorkTabH3_1;
+    document.getElementById("WorkTabP_1").innerText = t.WorkTabP_1;
+    document.getElementById("WorkTabH3_2").innerText = t.WorkTabH3_2;
+    document.getElementById("WorkTabP_2").innerText = t.WorkTabP_2;
+    document.getElementById("WorkTabH3_3").innerText = t.WorkTabH3_3;
+    document.getElementById("WorkTabP_3").innerText = t.WorkTabP_3;
+    document.getElementById("WorkTabH3_4").innerText = t.WorkTabH3_4;
+    document.getElementById("WorkTabP_4").innerText = t.WorkTabP_4;
+
     document.getElementById("sectionTechnologyH2").innerText = t.sectionTechnologyH2;  // Update the "Technology" section title
     document.getElementById("sectionTechnologyP").innerText = t.sectionTechnologyP;  // Update the "Technology" section description
   
@@ -93,7 +154,7 @@ const translations = {
     document.getElementById("divSkillsColumnH3Tools").innerText = t.divSkillsColumnH3.tools;  // Update the "Tools" skill title
     document.getElementById("sectionPortifolioH2").innerText = t.sectionPortifolioH2;
     document.getElementById("contactH2").innerText = t.contactH2;  // Update the "Contact" section title
-  
+    document.getElementById("rights").innerText = t.rights;
     // Update the flag image source and alternative text based on the selected language
     flag.src = language === "Português" ? "img/flags/pt.png" : "img/flags/en.png";  // Set the flag image to match the language
     flag.alt = language === "Português" ? "Brazil" : "USA";  // Set the alt text of the flag based on the selected language
